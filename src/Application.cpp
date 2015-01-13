b6af1c1a4c16322410e113b2b18313e6ea2abfab
jun.jiang <1669266647@qq.com>
2015-01-13 20:55:14 +0800

    Added some comment for design

--- src/Application.cpp
+++ src/Application.cpp
@@ -3,5 +3,13 @@
 
 INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR commandLine, INT)
 {
+	//Set update check info by register info
+
+	while (true)
+	{
+		//Wait SMP's connect
+		//Set update check info by the info that SMP sent
+	}
+
 	return 0;
 }

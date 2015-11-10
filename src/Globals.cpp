9833caaa1c636169fac879d2a82147c588000b45
jun.jiang <1669266647@qq.com>
2015-11-10 23:53:51 +0800

    AppName should be full name

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -2,7 +2,7 @@ const wchar_t* RPLAYER_PROCESS_NAME = L"RPlayer.exe";
 const char* REGISTER_INSTALLER_PATH = "InstallerPath";
 const char* FEED_URL = "http://downloads.sourceforge.net/project/smptest/appcast.xml";
 const wchar_t *COMPANY_NAME = L"PirateRabbit";
-const wchar_t *APP_NAME = L"RPlayer";
+const wchar_t *APP_NAME = L"RabbitPlayer";
 const char *REGISTER_PLAYER_VERSION = "PlayerVersion";
 const char* UPDATER_VERSION = "1.0.0";
 

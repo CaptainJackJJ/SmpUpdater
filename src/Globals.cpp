2368d233e0a0a0f49a3cef27dbd6fca398a8c15f
jun.jiang <1669266647@qq.com>
2015-11-15 18:22:52 +0800

    Reset Version to 1.0.0 to release the first rplayer

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -1,9 +1,9 @@
 const wchar_t* RPLAYER_PROCESS_NAME = L"RPlayer.exe";
 const char* REGISTER_INSTALLER_PATH = "InstallerPath";
-const char* FEED_URL = "http://downloads.sourceforge.net/project/smptest/appcast.xml";
+const char* FEED_URL = "http://downloads.sourceforge.net/project/piraterabbitplayer/appcast.xml";
 const wchar_t *COMPANY_NAME = L"PirateRabbit";
 const wchar_t *APP_NAME = L"RabbitPlayer";
 const char *REGISTER_APP_VERSION = "AppVersion";
-const char* UPDATER_VERSION = "1.0.5";
+const char* UPDATER_VERSION = "1.0.0";
 const wchar_t* UPDATER_NAME = L"RPUpdater";
 
